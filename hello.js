d8b65a071a657fed3e719fb6a46d5a3e3e223f68
yoshiki-17 <116180108+yoshiki-17@users.noreply.github.com>
2022-11-29 23:16:24 +0900

    commit
    js基礎学習

---
+++ hello.js
@@ -0,0 +1,19 @@
+//変数、定数宣言
+let price = 100 ;
+console.log(price * 1.08);
+
+const TAX = 1.08;
+console.log(price * TAX);
+
+//エスケープシーケンス
+
+console.log('He\' Hero');
+window.alert('こんにちはJavaScript ! \n 頑張って勉強しましょうね！')
+
+//テンプレート文字列
+
+let Name = '鈴木';
+let str = `こんにちは、${Name}さん。
+今日もいい天気ですね。`;
+
+console.log(str);
